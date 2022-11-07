71f0e5b57c55c9c640287c13d920e32efe084090
Sergey Denisyuk <sergey.denisyuk@mimy.online>
2022-11-07 15:50:52 +0200

    Refactor store

--- src/components/game/game.ts
+++ src/components/game/game.ts
@@ -1,110 +1,52 @@
 import {
-  CurrentPlayer,
+  changeBoardState,
+  changeRestartState,
+  changeStartState,
   GameStatus,
   store,
 } from '../../domain/stores/game.store'
-import { initUI } from './game-ui'
+import {
+  changeCellUI,
+  changeDrawUI,
+  changePlayerUI,
+  changeRestartUI,
+  changeStartUI,
+  changeWinUI,
+} from './game-ui'
 
 const playButton: HTMLElement | null = document.getElementById('play-btn')
 const replayButton: HTMLElement | null = document.getElementById('replay-btn')
 const recipeButton: HTMLElement | null = document.getElementById('recipe-btn')
 const cellNodes: NodeListOf<Element> = document.querySelectorAll('.cell')
 
-//Cell functions
 function handleCellClick(event: Event) {
-  const { gameState, status, currentPlayer } = store.get()
   const clickedCell: HTMLDivElement = event.target as HTMLDivElement
   const clickedValue: number | undefined = clickedCell.dataset.index
     ? Number(clickedCell.dataset.index)
     : undefined
 
-  if (
-    typeof clickedValue === 'undefined' ||
-    gameState[clickedValue] !== '' ||
-    status !== GameStatus.Running
-  ) {
-    return
+  changeCellUI(clickedCell)
+  changeBoardState(clickedCell, clickedValue)
+  changePlayerUI()
+
+  if (store.get().status === GameStatus.Win) {
+    changeWinUI()
   }
 
-  //update game state
-  const newGameState = [...gameState]
-  newGameState[clickedValue] = currentPlayer
-  store.setKey('gameState', newGameState)
-  store.setKey('node', clickedCell)
-
-  //check for result
-  handleResultValidation()
-}
-function handleResultValidation() {
-  const { winConditions, gameState, status } = store.get()
-
-  let roundWon = status === GameStatus.Win
-  for (let i = 0; i <= winConditions.length - 1; i++) {
-    //check if all values in win condition are same
-    const winCondition = winConditions[i]
-    let a = gameState[winCondition[0]]
-    let b = gameState[winCondition[1]]
-    let c = gameState[winCondition[2]]
-
-    //if all values are same and not empty
-    if (a === '' || b === '' || c === '') {
-      continue
-    }
-    //check if all cells are same
-    if (a === b && b === c) {
-      handleChangeWinState(winCondition)
-      roundWon = true
-      break
-    }
+  if (store.get().status === GameStatus.Draw) {
+    changeDrawUI()
   }
-
-  if (roundWon) {
-    return
-  }
-
-  if (!gameState.includes('')) {
-    handleChangeDrawState()
-    return
-  }
-
-  handlePlayerChange()
-}
-//End of Cell functions
-
-//Status changing functions
-function handleChangeWinState(winCondition: number[]) {
-  store.setKey('winCombination', winCondition)
-  store.setKey('status', GameStatus.Win)
-  localStorage.setItem('game_status', 'win')
-}
-
-function handleChangeDrawState() {
-  store.setKey('status', GameStatus.Draw)
 }
 
 function handleChangeStartState() {
-  store.setKey('status', GameStatus.Running)
+  changeStartState()
+  changeStartUI()
 }
 
 function handleChangeRestartState() {
-  store.setKey('status', GameStatus.Restart)
-  store.set({
-    ...store.get(),
-    currentPlayer: CurrentPlayer.X,
-    status: GameStatus.Running,
-    gameState: ['', '', '', '', '', '', '', '', ''],
-    winCombination: [],
-  })
-
+  changeRestartState()
+  changeRestartUI()
   localStorage.removeItem('game_status')
-}
-//End of Status changing functions
-
-function handlePlayerChange() {
-  const { currentPlayer } = store.get()
-  const updatedPlayer =
-    currentPlayer === CurrentPlayer.X ? CurrentPlayer.O : CurrentPlayer.X
-  store.setKey('currentPlayer', updatedPlayer)
 }
 
 function handleRedirectToDish() {
@@ -116,7 +58,6 @@ function init() {
     throw new Error('could not find elements')
   }
 
-  initUI()
   cellNodes.forEach((cell) => cell.addEventListener('click', handleCellClick))
   playButton.addEventListener('click', handleChangeStartState)
   replayButton.addEventListener('click', handleChangeRestartState)
--- src/domain/stores/game.store.ts
+++ src/domain/stores/game.store.ts
@@ -43,3 +43,89 @@ export const initialValues: GameStore = {
 }
 
 export const store = map<GameStore>(initialValues)
+
+export function changePlayer() {
+  const { currentPlayer } = store.get()
+  const updatedPlayer =
+    currentPlayer === CurrentPlayer.X ? CurrentPlayer.O : CurrentPlayer.X
+  store.setKey('currentPlayer', updatedPlayer)
+}
+
+export function changeWinState(winCondition: number[]) {
+  store.setKey('winCombination', winCondition)
+  store.setKey('status', GameStatus.Win)
+}
+
+export function changeDrawState() {
+  store.setKey('status', GameStatus.Draw)
+}
+
+export function changeStartState() {
+  store.setKey('status', GameStatus.Running)
+}
+
+export function changeRestartState() {
+  store.setKey('status', GameStatus.Restart)
+  store.set({
+    ...store.get(),
+    currentPlayer: CurrentPlayer.X,
+    status: GameStatus.Running,
+    gameState: ['', '', '', '', '', '', '', '', ''],
+    winCombination: [],
+  })
+}
+
+export function changeBoardState(element: Element, index?: number) {
+  const { gameState, status, currentPlayer } = store.get()
+
+  if (
+    typeof index === 'undefined' ||
+    gameState[index] !== '' ||
+    status !== GameStatus.Running
+  ) {
+    return
+  }
+
+  const newGameState = [...gameState]
+  newGameState[index] = currentPlayer
+  store.setKey('gameState', newGameState)
+  store.setKey('node', element)
+
+  validateResult()
+}
+
+function validateResult() {
+  const { winConditions, gameState, status } = store.get()
+
+  let roundWon = status === GameStatus.Win
+  for (let i = 0; i <= winConditions.length - 1; i++) {
+    //check if all values in win condition are same
+    const winCondition = winConditions[i]
+    let a = gameState[winCondition[0]]
+    let b = gameState[winCondition[1]]
+    let c = gameState[winCondition[2]]
+
+    //if all values are same and not empty
+    if (a === '' || b === '' || c === '') {
+      continue
+    }
+    //check if all cells are same
+    if (a === b && b === c) {
+      changeWinState(winCondition)
+      localStorage.setItem('game_status', 'win')
+      roundWon = true
+      break
+    }
+  }
+
+  if (roundWon) {
+    return
+  }
+
+  if (!gameState.includes('')) {
+    changeDrawState()
+    return
+  }
+
+  changePlayer()
+}
